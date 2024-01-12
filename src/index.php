d8d0ac32431978d9ed80a8566cd6796346fce883
Edwin Cromley <edwin@eridesign.com>
2024-01-12 15:09:05 -0500

    Implement example pull stream and sink.

--- src/index.php
+++ src/index.php
@@ -5,4 +5,38 @@
  * @package Koinonia
  */
 
-echo 'Loaded';
+namespace Koinonia;
+
+echo 'Loaded' . PHP_EOL;
+
+function values( $array ) {
+	$i = 0;
+
+	return function ( $abort, $cb ) use ( &$i, $array ) {
+		if ( $abort ) {
+			return $cb( $abort );
+		}
+
+		return $cb( $i >= count( $array ) ? true : null, isset( $array[ $i ] ) ? $array[ $i++ ] : null );
+	};
+}
+
+function log () {
+	return function ( $read ) {
+		$next = function ( $end, $data ) use ( $read, &$next ) {
+			if ( $end == true ) {
+				return;
+			} else if ( $end ) {
+				throw $end; // error.
+			}
+		
+			echo print_r( $data, true ) . PHP_EOL;
+		
+			$read( null, $next ); // loop again.
+		};
+
+    	$read( null, $next );
+	};
+}
+
+log()( values( [ 1, 2, 3, 4, 5 ] ) );
